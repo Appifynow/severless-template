b90679ca372635400437f2f70c4d5fde51da0475
dcoollx <dsqjr@msn.com>
2026-04-20 17:04:50 -0400

    backfill from leblis

---
+++ apps/backend/src/types/layer.d.ts
@@ -0,0 +1,13 @@
+// any types defined in the lambda layer should be added here
+import type { Stripe } from 'stripe';
+
+export {}
+
+declare module '/opt/nodejs/index' {
+
+  export interface CartResponse {
+    success: boolean;
+    paymentIntent: Stripe.PaymentIntent;
+  }
+  export function getStripeClient(key: string): Stripe;
+}
